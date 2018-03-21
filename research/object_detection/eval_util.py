00ce1834556cdb2ca9db118b3ab0fc1d5616c635
notname <notname@gmail.com>
2018-03-21 14:41:08 +0800

    Add gpu_options.allow_growth for eval

--- research/object_detection/eval_util.py
+++ research/object_detection/eval_util.py
@@ -280,7 +280,11 @@ def _run_checkpoint_once(tensor_dict,
   """
   if save_graph and not save_graph_dir:
     raise ValueError('`save_graph_dir` must be defined.')
-  sess = tf.Session(master, graph=tf.get_default_graph())
+  # Soft placement allows placing on CPU ops without GPU implementation.
+  session_config = tf.ConfigProto(allow_soft_placement=True,
+                                  log_device_placement=False)
+  session_config.gpu_options.allow_growth = True
+  sess = tf.Session(master, graph=tf.get_default_graph(), config=session_config)
   sess.run(tf.global_variables_initializer())
   sess.run(tf.local_variables_initializer())
   sess.run(tf.tables_initializer())
